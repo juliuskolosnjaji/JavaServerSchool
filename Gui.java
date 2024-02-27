b9cc76dff98b4a98b46f3ed83053aabe1145a339
julius <juliuskolosnjaji@outlook.de>
2024-02-27 14:22:12 +0100

    Add GUI Elements; Add start Server functionality

--- Gui.java
+++ Gui.java
@@ -4,13 +4,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Gui {
+    boolean ServerStarted = false;
+    static MeinServer ChatServer;
+    static JLabel LogLabel;
     public static void main(String[] args){
         JFrame Gui = new JFrame();
         Gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel Log = new JPanel();
-        Log.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        Gui.add(Log);
-        Log.setBounds(500, 500, 100, 100);
+        JPanel LogPanel = new JPanel();
+        LogPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        Gui.add(LogPanel);
+        LogPanel.setBounds(740, 100, 250, 450);
+
+        JPanel Chat = new JPanel();
+        Chat.setBorder(BorderFactory.createLineBorder(Color.black));
+        Gui.add(Chat);
+        Chat.setBounds(20, 30, 700, 440);
+
+        LogLabel = new JLabel();
 
         JButton Button = new JButton("Ligma");
         JButton ServerStart = new JButton("Server");
@@ -23,11 +33,21 @@ public class Gui {
             }
         });
 
+        ServerStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ChatServer = new MeinServer(69);
+                LogLabel.setText("Server started");
+                System.out.println("Server Started GUi");
+            }
+        });
+
         JTextField ChatText = new JTextField(20);
         ChatText.setOpaque(true);
-        ChatText.setBounds(30, 500, 700, 50);
+        ChatText.setBounds(20, 500, 700, 50);
 
-
+        LogPanel.add(LogLabel);
+        //Gui.add(LogLabel);
         Gui.add(Button);
         Gui.add(ChatText);
         Gui.add(ServerStart);
